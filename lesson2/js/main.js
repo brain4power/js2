bec60ff172039c5e627f60ac48206114a0099d19
brain4power <yotakrasnodar@yandex.ru>
2019-02-10 12:13:33 +0300

    добавил метод getSummaryCoast

--- lesson2/js/main.js
+++ lesson2/js/main.js
@@ -1,22 +1,33 @@
 class ProductsList {
-    constructor(container = '.products'){
+    constructor(container = '.products') {
         this.container = container;
         this.goods = [];
         this.allProducts = [];
         this._getProducts();
+        this.getSummaryCoast = this._getSummaryCoast();
     }
-    _getProducts(){
-       this.goods = [
-           {title: 'Notebook', price: 2000},
-           {title: 'Mouse', price: 20},
-           {title: 'Keyboard', price: 35},
-           {title: 'Gamepad', price: 48},
-           {title: 'Chair', price: 500},
-       ];
+
+    _getProducts() {
+        this.goods = [
+            {title: 'Notebook', price: 2000},
+            {title: 'Mouse', price: 20},
+            {title: 'Keyboard', price: 35},
+            {title: 'Gamepad', price: 48},
+            {title: 'Chair', price: 500},
+        ];
     }
-    render(){
+
+    _getSummaryCoast() {
+        let summary = 0;
+        this.goods.forEach(item => {
+            summary += item.price;
+        });
+        return summary
+    }
+
+    render() {
         const block = document.querySelector(this.container);
-        for (let product of this.goods){
+        for (let product of this.goods) {
             const productObj = new ProductItem(product.title, product.price);
             this.allProducts.push(productObj);
             block.insertAdjacentHTML('beforeend', productObj.render());
@@ -25,12 +36,13 @@ class ProductsList {
 }
 
 class ProductItem {
-    constructor(title, price, img = 'https://placehold.it/200x150'){
+    constructor(title, price, img = 'https://placehold.it/200x150') {
         this.title = title;
         this.price = price;
         this.img = img;
     }
-    render(){
+
+    render() {
         return `<div class="product-item">
                     <img src="${this.img}" alt="Some img">
                     <div class="desc">
