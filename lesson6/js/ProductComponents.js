dcfa491e2f0514a68231b2c25601c07c3e554f7c
brain4power <yotakrasnodar@yandex.ru>
2019-04-15 20:08:06 +0300

    почистил от лишнего кода.

--- lesson6/js/ProductComponents.js
+++ lesson6/js/ProductComponents.js
@@ -53,31 +53,4 @@ Vue.component('product', {
                 </div>
             </div>
         `
-})
-
-
-// Vue.component('products', {
-//     props: ['products', 'img'],
-//     template: `
-//     <div class="products">
-//             <product v-for="item of products"
-//             :product="item" :img="img" :key="item.product_name">
-//            </product>
-//         </div>
-//     `
-// });
-//
-// Vue.component('product', {
-//     props: ['product', 'img'],
-//     template: `
-//     <div class="product-item">
-//                 <img :src="img" alt="Some img">
-//                 <div class="desc">
-//                     <h3>{{ product.product_name }}</h3>
-//                     <p>$ {{ product.price }}</p>
-//                     <button class="buy-btn"
-//                     @click="$parent.$emit('add-product', product)">Купить</button>
-//                 </div>
-//             </div>
-//     `
-// })
+});
